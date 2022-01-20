8ba32c9ad31faffac589715d1eaf4f75c6794d4e
Rafael Aguiar <rafaelaguiar@unitins.br>
2022-01-20 20:14:51 -0300

    Nav bar e footer feitos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import {BrowserRouter as Router,Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch, Route, Link} from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
+
+import Container from './components/layout/Container'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
+      <NavBar/>
       <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/company' >
-          <Company />
-        </Route>
-        <Route path='/contact' >
-          <Contact />
-        </Route>
-        <Route path='/newproject' >
-          <NewProject />
-        </Route>
-
-
+        <Container customClass='min-height'>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/Projects' >
+              <Projects/>
+            </Route>
+            <Route path='/company' >
+              <Company />
+            </Route>
+            <Route path='/contact' >
+              <Contact />
+            </Route>
+          
+          </Container>
       </Switch>
-
+      <Footer/>
 
     </Router>
   );
