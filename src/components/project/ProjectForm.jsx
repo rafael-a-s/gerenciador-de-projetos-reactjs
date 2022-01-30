527ded6d0cf15257c89d1d107ce7a056824da5c4
Rafael Aguiar <rafaelaguiar@unitins.br>
2022-01-30 18:46:07 -0300

    Adicionado o banco de dados, na file db.json, API rest: componentes do formulario criados.

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,20 +1,34 @@
-function ProjectForm(){
+import { useEffect, useState } from 'react'
+
+import Input from '../../form/Input'
+import Select from '../../form/Select'
+import SubmitButton from '../../form/SubmitButton'
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}){
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/categories',{
+  method: "GET",
+  headers:{
+    'content-type':'application/json'
+  }
+  })
+  .then((resp) => resp.json())
+  .then((data) => {
+    setCategories(data)
+  })
+  .catch((err) => console.log(err))
+  }, [])
+  
   return (
-    <form >
-      <div>
-      <input type="text"  placeholder="Insira o nome do projeto"/>
-      </div>
-     <div>
-       <input type="number"  placeholder="Insira o nome do projeto"/>
-     </div>
-      <div>
-      <select name="category_id" >
-        <option disabled selected>Selecione a categoria</option>
-      </select>
-      </div>
-     <div>
-       <input type="submit"  value='Criar projeto'/>
-       </div>
+
+    <form  className={styles.form}>
+      <Input type='text' text='nome do projeto' name='name' placeholder='Insira o nome do projeto' />
+
+      <Input type='number' text='orçamento do projeto' name='budget' placeholder='Insira o orçamento total' />
+      <Select name='category_id' text='Selecione uma categoria' options={categories}/>
+     <SubmitButton text={btnText}/>
     </form>
   )
 }
