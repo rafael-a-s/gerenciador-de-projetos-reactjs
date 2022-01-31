2ea6fa38e372e71329cb39c966e7c7c4311b0349
Rafael Aguiar <rafaelaguiar@unitins.br>
2022-01-31 10:07:58 -0300

    metodo get e post feitos, porem está com erro na hora de fazer o post, falta concertar

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,11 +1,34 @@
+import { useHistory } from 'react-router-dom'
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
 function NewProject(){
+
+const history = useHistory()
+
+function createPost(project){
+  //initialize o cost
+  project.cost = 0
+  project.services = []
+
+  fetch('http://localhost:5000/projects',{
+    method:"POST",
+    headers:{
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(project),
+  }).then((resp) => resp.json())
+  .then((data) =>{
+    console.log(data,'data')
+    history.push('/projects', {message: 'Projeto criado com sucesso'}) 
+  })
+.catch((err) => console.log(err))
+}
+
   return (
     <div className={styles.newproject_container}>
       <h1 >Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText='Criar projeto'/>
+      <ProjectForm hanleSubmit={createPost} btnText='Criar projeto'/>
 
     </div>
   )
