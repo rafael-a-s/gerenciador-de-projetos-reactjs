ffb19a59a015469983c951b95263f30f80cac6d0
Rafael Aguiar <rafaelaguiar@unitins.br>
2022-02-02 09:51:57 -0300

    Cards feitos junto com a busca de projetos no backend, e componte de loading feito tambem.

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,14 +2,38 @@ import { useLocation } from "react-router-dom"
 import Message from "../layout/Message"
 import styles from './Projects.module.css'
 import Container from '../layout/Container'
+import Loading from "../layout/Loading"
 import LinkButton from '../layout/LinkButton'
-
+import ProjectCard from "../project/ProjectCard"
+import {useState, useEffect} from 'react'
 function Projects(){
+  const [removeLoading, setRemoveLoading] = useState(false)
+
+  const [projects, setProjects] = useState([])
+
   const location = useLocation()
   let message = ''
   if (location.state){
     message = location.state.message
   }
+
+  useEffect( () => {
+    setTimeout( () => {
+      fetch('http://localhost:5000/projects',{
+      method:'GET',
+      headers:{
+        'Content-Type' : 'application/json',
+      },
+    }).then((resp )=> resp.json())
+    .then((data) => {
+      console.log(data)
+      setProjects(data)
+      setRemoveLoading(true)
+    })
+    .catch((err) => console.log(err))
+    }, 300)
+    
+  }, [])
   return (
         <div className={styles.project_container}>
           <div className={styles.title_container}>
@@ -19,7 +43,20 @@ function Projects(){
          
           {message &&  <Message type='success' msg={message} />}
           <Container customClass='start'>
-            <p>projetos</p>
+            {projects.length > 0 &&
+              projects.map((project) => (
+                <ProjectCard 
+                name={project.name}
+                id={project.id}
+                budget={project.budget}
+                category={project.category.name}
+                key={project.id}/>
+              ))
+            }
+            {!removeLoading && <Loading/>}
+            {removeLoading && projects.length == 0(
+                <p>Não há projetos cadastrados</p>
+            )}
           </Container>
         </div>
     )
